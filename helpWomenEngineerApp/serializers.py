cbde441670ac4dbfce86cd081bab3b6039c271bb
= <lintzu@oregonstate.edu>
2017-06-19 00:12:55 -0700

    Remove Redundant Folders

--- helpWomenEngineerApp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from helpWomenEngineerApp.models import StigmasFromReddit
-
-class dataSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = StigmasFromReddit
-		fields = ('id', 'DateOfCreation', 'subReddit', 'message')
-
